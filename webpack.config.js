efc97a11ca98e38c42ec1d28c4a4fba26ba2b281
范轶洁 <fanyjb@digitalchina.com>
2022-04-26 15:47:01 +0800

    es2015

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  target: ["web", "es5"],
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "管理输出",
+    }),
+  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
 };
